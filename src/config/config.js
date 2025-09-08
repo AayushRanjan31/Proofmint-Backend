1866675fa13927c98eff9785ad54481f6724171e
Sanni Kumar <aryansingh7371@gmail.com>
2025-09-08 14:43:24 +0530

    added the email user and password in the env file

--- src/config/config.js
+++ src/config/config.js
@@ -9,4 +9,7 @@ module.exports = {
   secretKey: process.env.DB_SECRET_KEY,
   jwtKey: process.env.JWT_SECRET_KEY,
   cookies: process.env.NODE_ENV,
+  userEmail: process.env.EMAIL_USER,
+  emailPassword: process.env.EMAIL_PASSWORD,
+  qrBase: process.env.QR_BASE_URL,
 };
