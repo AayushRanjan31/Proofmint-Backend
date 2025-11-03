efa68efe1f4f900f111eaf7ee9225437ab9afe37
Aayush Ranjan <110541448+AayushRanjan31@users.noreply.github.com>
2025-11-03 20:55:16 +0530

    Add new allowed CORS origin for production

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:5175',
     'https://ac4087f1a82c.ngrok-free.app',
+   'https://proofmint.up.railway.app',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
